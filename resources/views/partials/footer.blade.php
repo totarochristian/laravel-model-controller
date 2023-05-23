e32c59f22abeb8e017bd70d033eeabd54cb8f78f
totarochristian <totaro.christian.18@gmail.com>
2023-05-23 15:33:00 +0200

    Update styles

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,5 @@
-<div class="w-100 bg-dark text-light text-center p-4">
-    <span>Made with &hearts; by <b>Christian Totaro</b></span>
-</div>
+<footer class="w-100">
+    <div class="w-100 bg-dark text-light text-center p-4">
+        <span>Made with &hearts; by <b>Christian Totaro</b></span>
+    </div>
+</footer>
