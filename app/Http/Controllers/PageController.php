a31b4f70f29cd00bd85031c35168fdca3c83d323
totarochristian <totaro.christian.18@gmail.com>
2023-05-23 15:20:47 +0200

    Add the show function in the page controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,12 @@ class PageController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-
         return view('movies.index',compact('movies'));
+    }
+
+    public function show($id)
+    {
+        $movie = Movie::findOrFail($id);
+        return view('movies.show', compact('movie'));
     }
 }
