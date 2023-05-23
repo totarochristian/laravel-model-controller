93b7a4349b259bc0cb80fe4ab4fbdbe7adbcdccf
totarochristian <totaro.christian.18@gmail.com>
2023-05-23 14:44:26 +0200

    Update page controller adding index function that will return all the movie in the db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all();
+
+        return view('welcome',compact('movies'));
+    }
 }
